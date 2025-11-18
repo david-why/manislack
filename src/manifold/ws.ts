34bdbbc5152cacbca7101f5fd3dc8849754655e8
david-why <david_why@outlook.com>
2025-11-18 10:09:56 +0800

    added types and reconnect logic

--- src/manifold/ws.ts
+++ src/manifold/ws.ts
@@ -1,3 +1,5 @@
+import { EventEmitter } from 'events'
+
 export type ManifoldSubscribeTopic =
   | 'global/new-bet'
   | 'global/new-contract'
@@ -16,6 +18,8 @@ export type ManifoldSubscribeTopic =
   | `user/${number}`
   | `answer/${number}/update`
   | 'tv_schedule'
+
+// outgoing messages
 
 export interface ManifoldMessageBase {
   txid: number
@@ -40,17 +44,61 @@ export type ManifoldOutgoingMessage =
   | ManifoldSubscribeMessage
   | ManifoldUnsubscribeMessage
 
-export class ManifoldWebSocket {
+// incoming messages
+
+export interface ManifoldAckMessage extends ManifoldMessageBase {
+  type: 'ack'
+  success: boolean
+}
+
+interface ManifoldBroadcastMessageTempl<
+  Topic extends ManifoldSubscribeTopic = any,
+> extends ManifoldMessageBase {
+  type: 'broadcast'
+  topic: Topic
+  data: ManifoldBroadcastData[Topic]
+}
+
+type ManifoldMessageMap = {
+  [K in ManifoldSubscribeTopic]: ManifoldBroadcastMessageTempl<K>
+}
+
+export type ManifoldBroadcastMessage =
+  ManifoldMessageMap[keyof ManifoldMessageMap]
+
+export type ManifoldBroadcastData = Record<`contract/${number}`, ''> &
+  Record<ManifoldSubscribeTopic, unknown>
+
+export type ManifoldIncomingMessage =
+  | ManifoldAckMessage
+  | ManifoldBroadcastMessage
+
+// websocket stuff
+
+type ManifoldWebSocketEventMap = {
+  ack: [txid: number]
+  broadcast: [message: ManifoldBroadcastMessage]
+}
+
+const PING_TIMEOUT = 10000
+const RECONNECT_WAIT = 1000 // TODO: exponential backoff
+
+export class ManifoldWebSocket extends EventEmitter<ManifoldWebSocketEventMap> {
   public ws: WebSocket
-  private wsInterval?: ReturnType<typeof setInterval>
+  private pingInterval?: ReturnType<typeof setInterval>
+  private reconnectTimeout?: ReturnType<typeof setTimeout>
   private txid = 0
 
   constructor() {
+    super()
     this._onClose = this._onClose.bind(this)
     this._onOpen = this._onOpen.bind(this)
     this._onError = this._onError.bind(this)
     this._onMessage = this._onMessage.bind(this)
+    this._send = this._send.bind(this)
+    this._ping = this._ping.bind(this)
     this.ws = this._createWS()
+    console.log('ManifoldWebSocket initialized')
   }
 
   private _createWS() {
@@ -63,38 +111,86 @@ export class ManifoldWebSocket {
   }
 
   private async _send(message: Omit<ManifoldOutgoingMessage, 'txid'>) {
-    this.ws.send(JSON.stringify(message))
-    return new Promise((resolve, reject) => {
+    const txid = this.txid++
+    const payload = Object.assign({ txid }, message)
 
+    this.ws.send(JSON.stringify(payload))
+
+    return new Promise<void>((resolve, reject) => {
+      const handler = (ackTxid: number) => {
+        if (txid === ackTxid) {
+          this.off('ack', handler)
+          clearTimeout(timeout)
+          resolve()
+        }
+      }
+
+      const timeout = setTimeout(() => {
+        console.warn(
+          `Server did not respond to ping in ${PING_TIMEOUT}ms, reconnecting`,
+        )
+        this.ws.close(1000, 'Ping timeout')
+        this.off('ack', handler)
+        reject(new Error('Ping timeout'))
+      }, PING_TIMEOUT)
+
+      this.on('ack', handler)
     })
   }
 
   private async _ping() {
-    console.log('Sending websocket ping...')
-    return this._send({ type: 'ping' })
+    console.log('Sending websocket ping at', new Date())
+    const start = Date.now()
+    await this._send({ type: 'ping' })
+    const end = Date.now()
+    console.log(`Received pong in ${end - start}ms`)
   }
 
   // websocket events
 
   private _onClose(ev: CloseEvent) {
     const ws = ev.target as WebSocket
+    if (ws != this.ws) return
     console.log('ws closed', ev.code, ev.reason, ev.wasClean)
     ws.removeEventListener('close', this._onClose)
     ws.removeEventListener('open', this._onOpen)
     ws.removeEventListener('error', this._onError)
     ws.removeEventListener('message', this._onMessage)
+    this._reconnect()
   }
   private _onOpen(ev: Event) {
+    const ws = ev.target as WebSocket
+    if (ws != this.ws) return
     console.log('ws opened')
-    if (this.wsInterval) {
-      clearInterval(this.wsInterval)
+    if (this.pingInterval) {
+      clearInterval(this.pingInterval)
     }
-    this.wsInterval = setInterval(this._ping, 30000)
+    this.pingInterval = setInterval(this._ping, 30000)
   }
   private _onError(ev: Event) {
-    console.log('ws error:', ev.target, ev)
+    const ws = ev.target as WebSocket
+    if (ws != this.ws) return
+    console.log('ws error:', ws, ev)
+    this._reconnect()
   }
-  private _onMessage(ev: Bun.MessageEvent) {
+  private _onMessage(ev: Bun.MessageEvent<string>) {
     console.log('message:', ev.data)
+    const payload = JSON.parse(ev.data) as ManifoldIncomingMessage
+    if (payload.type === 'ack') {
+      this.emit('ack', payload.txid)
+    } else if (payload.type === 'broadcast') {
+      this.emit('broadcast', payload)
+    }
+  }
+
+  private _reconnect() {
+    if (this.reconnectTimeout) return
+    try {
+      this.ws.close(1000, 'Reconnect requested')
+    } catch {}
+    this.reconnectTimeout = setTimeout(() => {
+      this.reconnectTimeout = undefined
+      this.ws = this._createWS()
+    }, RECONNECT_WAIT)
   }
 }
